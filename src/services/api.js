28304fd70bdb4812b37a62d6d1e22d9936d690d5
jatinkakadiya <jatinkakadiya234@gmail.com>
2025-12-09 15:03:07 +0530

    init

--- src/services/api.js
+++ src/services/api.js
@@ -1,87 +1,88 @@
-import axios from 'axios'
+import axios from "axios";
 
-const API_BASE_URL = 'http://localhost:8888/api'
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
-    'Content-Type': 'application/json'
-  }
-})
+    "Content-Type": "application/json",
+  },
+});
+
 
 class ApiService {
   async registerUser(userData) {
-    const { data } = await api.post('/user/register', userData)
-    return data
+    const { data } = await api.post("/user/register", userData);
+    return data;
   }
 
   async loginUser(credentials) {
-    const { data } = await api.post('/user/login', credentials)
-    return data
+    const { data } = await api.post("/user/login", credentials);
+    return data;
   }
 
   async getTemplates() {
-    const { data } = await api.get('/templates')
-    return data
+    const { data } = await api.get("/templates");
+    return data;
   }
 
   async getTemplateById(id) {
-    const { data } = await api.get(`/templates/${id}`)
-    return data
+    const { data } = await api.get(`/templates/${id}`);
+    return data;
   }
 
   async sendMessage(campaignData) {
-   return  await api.post('/user/sendMessage', campaignData)
+    return await api.post("/user/sendMessage", campaignData);
   }
   async chackcapebalNumber(phoneNumbers) {
-    return await api.post('/user/checkAvablityNumber', {phoneNumbers})
+    return await api.post("/user/checkAvablityNumber", { phoneNumbers });
   }
 
   async uploadFile(file) {
-    const formData = new FormData()
-    formData.append('file', file)
-    
-    const { data } = await api.post('/user/uploadFile', formData, {
+    const formData = new FormData();
+    formData.append("file", file);
+
+    const { data } = await api.post("/user/uploadFile", formData, {
       headers: {
-        'Content-Type': 'multipart/form-data'
-      }
-    })
-    return data
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    return data;
   }
 
   async getMessageReportsall() {
-    const { data } = await api.get('/message-reports/alls')
-    return data
+    const { data } = await api.get("/message-reports/alls");
+    return data;
   }
 
   async getMessageById(phoneNumber) {
-    const { data } = await api.get(`/message-reports/check/${phoneNumber}`)
-    return data
+    const { data } = await api.get(`/message-reports/check/${phoneNumber}`);
+    return data;
   }
   async getMessageReports() {
-    const { data } = await api.get('/message-reports/report')
-    return data
+    const { data } = await api.get("/message-reports/report");
+    return data;
   }
 
   async deleteMessageReport(id) {
-    const { data } = await api.delete(`/message-reports/${id}`)
-    return data
+    const { data } = await api.delete(`/message-reports/${id}`);
+    return data;
   }
 
   async createTemplate(templateData) {
-    const { data } = await api.post('/templates', templateData)
-    return data
+    const { data } = await api.post("/templates", templateData);
+    return data;
   }
 
   async updateTemplate(id, templateData) {
-    const { data } = await api.put(`/templates/${id}`, templateData)
-    return data
+    const { data } = await api.put(`/templates/${id}`, templateData);
+    return data;
   }
 
   async deleteTemplate(id) {
-    const { data } = await api.delete(`/templates/${id}`)
-    return data
+    const { data } = await api.delete(`/templates/${id}`);
+    return data;
   }
 }
 
-export default new ApiService()
+export default new ApiService();
