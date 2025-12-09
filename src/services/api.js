f73576f9d0e37546022c95004c31c8b912f9ec4b
jatinkakadiya <jatinkakadiya234@gmail.com>
2025-12-09 15:40:48 +0530

    cdaa

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://rcssender.com";
+const API_BASE_URL = "https://rcssender.com/api/v1";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
