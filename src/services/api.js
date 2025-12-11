9b9d089ff241d976bab82f7b6fbdbe0dc3b59ffd
jatinkakadiya <jatinkakadiya234@gmail.com>
2025-12-11 18:20:17 +0530

    init

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { getCookie } from "../utils/cookieUtils";
 
-const API_BASE_URL = "https://rcssender.com";
+const API_BASE_URL = "https://rcssender.com/api";
 // const API_BASE_URL = "http://localhost:8888";
 
 const api = axios.create({
@@ -23,42 +23,42 @@ api.interceptors.request.use((config) => {
 
 class ApiService {
   async registerUser(userData) {
-    const { data } = await axios.post(API_BASE_URL + "/api/v1/user/register", userData);
+    const { data } = await api.post("/v1/user/register", userData);
     return data;
   }
 
   async loginUser(credentials) {
-    const { data } = await axios.post(API_BASE_URL + "/api/v1/user/login", credentials);
+    const { data } = await api.post("/v1/user/login", credentials);
     return data;
   }
 
   async getTemplates() {
-    const { data } = await axios.get(API_BASE_URL +   "/api/v1/templates");
+    const { data } = await api.get("/v1/templates");
     return data;
   }
 
   async getUserTemplates(userId) {
-    const { data } = await axios.get(API_BASE_URL + `/api/v1/templates/user/${userId}`);
+    const { data } = await api.get(`/v1/templates/user/${userId}`);
     return data;
   }
 
   async getTemplateById(id) {
-    const { data } = await axios.get(API_BASE_URL + `/api/v1/templates/${id}`);
+    const { data } = await api.get(`/v1/templates/${id}`);
     return data;
   }
 
   async sendMessage(campaignData) {
-    return await axios.post(API_BASE_URL + "/api/v1/user/sendMessage", campaignData);
+    return await api.post("/v1/user/sendMessage", campaignData);
   }
   async chackcapebalNumber(phoneNumbers, userId) {
-    return await axios.post(API_BASE_URL + "/api/v1/user/checkAvablityNumber", { phoneNumbers,userId });
+    return await api.post("/v1/user/checkAvablityNumber", { phoneNumbers,userId });
   }
 
   async uploadFile(file) {
     const formData = new FormData();
     formData.append("file", file);
 
-    const { data } = await axios.post(API_BASE_URL + "/api/v1/user/uploadFile", formData, {
+    const { data } = await api.post("/v1/user/uploadFile", formData, {
       headers: {
         "Content-Type": "multipart/form-data",
       },
@@ -67,60 +67,60 @@ class ApiService {
   }
 
   async getMessageReportsall(userId) {
-    const { data } = await axios.get(API_BASE_URL + `/api/v1/message-reports/alls/${userId}`);
+    const { data } = await api.get(`/v1/message-reports/alls/${userId}`);
     return data;
   }
 
   async getMessageById(phoneNumber) {
-    const { data } = await axios.get(API_BASE_URL + `/api/v1/message-reports/check/${phoneNumber}`);
+    const { data } = await api.get(`/v1/message-reports/check/${phoneNumber}`);
     return data;
   }
   async getMessageReports() {
-    const { data } = await axios.get(API_BASE_URL + "/api/v1/message-reports/report");
+    const { data } = await api.get("/v1/message-reports/report");
     return data;
   }
 
   async deleteMessageReport(id) {
-    const { data } = await axios.delete(API_BASE_URL + `/api/v1/message-reports/${id}`);
+    const { data } = await api.delete(`/v1/message-reports/${id}`);
     return data;
   }
 
   async createTemplate(templateData) {
-    const { data } = await axios.post(API_BASE_URL + "/api/v1/templates", templateData);
+    const { data } = await api.post("/v1/templates", templateData);
     return data;
   }
 
   async updateTemplate(id, templateData) {
-    const { data } = await axios.put(API_BASE_URL + `/api/v1/templates/${id}`, templateData);
+    const { data } = await api.put(`/v1/templates/${id}`, templateData);
     return data;
   }
 
   async deleteTemplate(id) {
-    const { data } = await axios.delete(API_BASE_URL + `/api/v1/templates/${id}`);
+    const { data } = await api.delete(`/v1/templates/${id}`);
     return data;
   }
 
   async getUserMessages(userId) {
-    const { data } = await axios.get(API_BASE_URL + `/api/v1/user/messages/${userId}`);
+    const { data } = await api.get(`/v1/user/messages/${userId}`);
     return data;
   }
 
   async getAllUsers() {
-    const { data } = await axios.get(API_BASE_URL + '/api/v1/user/users');
+    const { data } = await api.get('/v1/user/users');
     return data;
   }
 
   async addWalletRequest(requestData) {
-    const { data } = await axios.post(API_BASE_URL + "/api/v1/user/wallet/request", requestData);
+    const { data } = await api.post("/v1/user/wallet/request", requestData);
     return data;
   }
   async getrecentorders(userId) {
-    const { data } = await axios.get(API_BASE_URL + `/api/v1/message-reports/recent/${userId}`);
+    const { data } = await api.get(`/v1/message-reports/recent/${userId}`);
     return data;
   }
 
   async getMessageStats(userId) {
-    const { data } = await axios.get(API_BASE_URL + `/api/v1/message-reports/stats/${userId}`);
+    const { data } = await api.get(`/v1/message-reports/stats/${userId}`);
     return data;
   }
 }
