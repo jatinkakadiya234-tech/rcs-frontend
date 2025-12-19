7bf0ae6b92fc8c4df730b27bdefeb295ac460c90
jatinkakadiya <jatinkakadiya234@gmail.com>
2025-12-19 13:03:20 +0530

    sdfg

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import { getCookie } from "../utils/cookieUtils";
 
-// const API_BASE_URL = "https://rcssender.com/api";
-const API_BASE_URL = "http://localhost:8888/api";
+const API_BASE_URL = "https://rcssender.com/api";
+// const API_BASE_URL = "http://localhost:8888/api";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
